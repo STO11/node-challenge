cf436710aad2dbde8d1d0d1cd6853cc850be9a9a
Suelton <sueltonlima@gmail.com>
2020-12-23 23:03:55 -0300

    feat(.env): add .env variables in config and add package ts-dotenv

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,11 @@
-const urlBaseReciPepuppy = 'http://recipepuppy.com/api';
-const urlBaseGiphy = 'http://api.giphy.com/v1/gifs/search';
+import { load } from 'ts-dotenv';
+
+const env = load({
+  GIPHY_KEY: String,
+  BASEURLGIPHY: String,
+  BASEURLRECIPEPUPPY: String,
+});
+
+const urlBaseReciPepuppy = env.BASEURLRECIPEPUPPY;
+const urlBaseGiphy = env.BASEURLGIPHY;
 export { urlBaseReciPepuppy, urlBaseGiphy };
